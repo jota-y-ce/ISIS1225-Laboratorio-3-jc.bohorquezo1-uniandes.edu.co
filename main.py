440296488e888c7226f2b76be7b2193b3a26c656
jota-y-ce <145022052+jota-y-ce@users.noreply.github.com>
2024-09-12 02:04:51 +0000

    mod main

--- main.py
+++ main.py
@@ -1,11 +1,1 @@
-import App.view as view
-
-
-# Main function
-def main():
-    view.main()
-
-
-# Main function call to run the program
-if __name__ == '__main__':
-    main()
+print("Holaaaaaaaaaaaaaaaaaaaaaaa")
